c2579328f577f92e3b943041d4624c1005ae1ec3
holyyOne <147740695+holyyOne@users.noreply.github.com>
2025-06-03 14:42:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    greeting = "Hello, World!"
+    greeting = "Lmaooo"
     if request.method == "POST":
         name = request.form.get("name")
         if name:
