d54a5b678d4913a7dabee5a5526c4c12ddd6fea1
holyyOne <147740695+holyyOne@users.noreply.github.com>
2025-06-03 14:01:41 +0700

    Delete simple-flask-app directory

--- simple-flask-app/static/scrips.js
+++
@@ -1,5 +0,0 @@
-async function showMessage() {
-    const response = await fetch('/click', { method: 'POST' });
-    const data = await response.json();
-    document.getElementById('message').innerText = data.message;
-}
